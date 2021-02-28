ede870f7008d9380b957984f88b6fa213f72aed3
smorales95 <steven_pibe95@hotmail.com>
2021-02-28 02:16:52 +0000

    Ejercicio Resuelto

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,25 +1,28 @@
-window.onload = function() {
+window.onload = function loadFn() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 	// your code here
-	let select = document.getElementById("#mySelect");
+	let select = document.getElementById("mySelect");
 	let opt = document.getElementById("#opt");
 
 	let btn = document.getElementById("#btn");
+	let conte = document.getElementById("container");
 	cargar();
+
 	function cargar() {
 		for (var i = 0; i < countries.length; i++) {
 			//  select.options[i] = new Option(countries[i]);
 			var elemp = document.createElement("option");
 			elemp.innerHTML = countries[i];
+			elemp.value = countries[i];
 			document.querySelector("#mySelect").appendChild(elemp);
 		}
 	}
+	select.addEventListener("change", function(event) {
+		//let val = this.options[select.selectedIndex];
+		var selectedOption = this.options[select.selectedIndex];
 
-	select.addEventListener("change", function() {
-		alert(select.getElementsByTagName);
-
-		console.log("hola");
+		alert(selectedOption.value);
 	});
 };
